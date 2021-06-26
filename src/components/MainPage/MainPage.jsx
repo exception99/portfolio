11b1081a68fe74b1e3feccbc8b4a5fcfb01f128a
Abhishek Aman <aaman@grepruby.io>
2021-06-27 03:19:52 +0530

    Started this Project

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+function MainPage() {
+    return (
+        <div>
+            <Header />
+            <h1>This is the content Section.</h1>
+        </div>
+    )
+}
+
+export default MainPage
